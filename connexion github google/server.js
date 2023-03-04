1bc05ce9a56d2d9b037fa0f0f104b6b3d317e902
jarod25 <kohler.jarod.2004@gmail.com>
2023-03-04 16:14:45 +0100

    Wola jpense ça marche presque

--- connexion github google/server.js
+++ connexion github google/server.js
@@ -1,11 +1,16 @@
-var express = require('express');
-var app = express();
-var bodyParser = require('body-parser');
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const GitHubStrategy = require('passport-github').Strategy;
 const jwt = require('jsonwebtoken');
-const { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, GITHUB_CLIENT_ID, GITHUB_CLIENT_SECRET, JWT_SECRET } = require('dotenv').config();
+require('dotenv').config();
+const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
+const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
+const GITHUB_CLIENT_ID = process.env.GITHUB_CLIENT_ID;
+const GITHUB_CLIENT_SECRET = process.env.GITHUB_CLIENT_SECRET;
+
 
 // Configurer les stratégies de connexion avec Google et GitHub
 passport.use(new GoogleStrategy({
@@ -50,3 +55,7 @@ app.get('/auth/github/callback',
     function(req, res) {
         // Générer un JWT pour l'utilisateur et l'envoyer dans la réponse HTTP
     });
+
+app.listen(3000, () => {
+    console.log(`Example app listening at http://localhost:${3000}`);
+});
