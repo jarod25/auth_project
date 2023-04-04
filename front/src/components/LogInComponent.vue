33182290e878a81b3ae4c6356a24e441b7a30f6a
jarod25 <kohler.jarod.2004@gmail.com>
2023-04-04 23:21:05 +0200

    ajout du front (parce que l'autre avant a eu des problèmes)

---
+++ front/src/components/LogInComponent.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="login">
+    <div class="login__container">
+      <div class="login__container__title">
+        <h1>Log In</h1>
+      </div>
+      <div class="login__container__form">
+        <form action="/auth/login">
+          <div class="login__container__form__input">
+            <label for="email">Email</label>
+            <input type="email" name="email" id="email">
+          </div>
+          <div class="login__container__form__input">
+            <label for="password">Password</label>
+            <input type="password" name="password" id="password">
+          </div>
+          <div class="login__container__form__input">
+            <button type="submit">Log In</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "LogInComponent",
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ front/src/components/NavBarComponent.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="navbar">
+    <div class="navbar__logo">
+      <img src="../assets/logo.png" alt="logo">
+    </div>
+    <div class="navbar__menu">
+      <ul>
+        <li><router-link to="/">Home</router-link></li>
+        <li><router-link to="/login">Login</router-link></li>
+        <li><router-link to="/signup">Signup</router-link></li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NavBarComponent.vue",
+
+}
+</script>
+
+<style scoped>
+.navbar {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: #383737;
+}
+
+
+.navbar__logo img {
+  width: 20%;
+}
+
+.navbar__menu {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.navbar__menu ul {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  list-style: none;
+}
+
+.navbar__menu ul li {
+  margin: 0 1rem;
+}
+
+.navbar__menu ul li a {
+  color: #fff;
+  text-decoration: none;
+}
+</style>
